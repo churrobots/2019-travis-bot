9af57fcbda549cb0d9743e28339f7b7f7274521f
EPA Robotics <>
2019-09-28 11:35:59 -0700

    Update CargoCannon.java
    Co-Authored-By: Matt Pizzimenti <mjpizz@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/CargoCannon.java
+++ src/main/java/frc/robot/subsystems/CargoCannon.java
@@ -10,6 +10,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
@@ -48,8 +49,9 @@ public class CargoCannon extends Subsystem {
     _hook.set(false);
   }
   public void lowerConveyor(){
-    _wrist
+    _wrist.set(Value.kForward);
   }
+  
   
 
 }
